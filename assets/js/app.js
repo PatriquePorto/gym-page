54ef0187e0e185f294a26d437fa0f4dd06ff7bfd
Patrique Porto <patrikrock@hotmail.com>
2023-07-31 18:02:28 -0300

    Add responsive details

---
+++ assets/js/app.js
@@ -0,0 +1,12 @@
+function calcularMedia(notas) {
+    let soma = 0
+    for (let i = 0; i <= notas.length; i++) {
+        soma += notas[i]
+    }
+    let media = soma / notas.length
+    return media
+}
+
+const notasAluno = [7, 8, 9, 6, 5]
+const mediaFinal = calcularMedia(notasAluno)
+console.log("A média do aluno é: " + mediaFinal)
